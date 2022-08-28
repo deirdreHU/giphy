9db3acf6e408ec4f643a4d02aa7be3ad93abbb35
Deirdre Hu <deirdre.hu@edoovo.com>
2022-08-28 08:16:27 +0800

    deploy

---
+++ build/precache-manifest.968ffaa990208dc1d4d20006c6ef38a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8280629f4990fed15e38ce61987a7e",
+    "url": "/giphy/index.html"
+  },
+  {
+    "revision": "20838deeeb9172304859",
+    "url": "/giphy/static/css/main.fe1fb73a.chunk.css"
+  },
+  {
+    "revision": "922a9784c1f065754a9c",
+    "url": "/giphy/static/js/2.5cd29ab0.chunk.js"
+  },
+  {
+    "revision": "20838deeeb9172304859",
+    "url": "/giphy/static/js/main.9192cde1.chunk.js"
+  },
+  {
+    "revision": "2be08ba7eaf0823a821e",
+    "url": "/giphy/static/js/runtime~main.f83d3e2c.js"
+  }
+]);
